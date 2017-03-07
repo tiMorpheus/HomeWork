d98b69c5c665569f35cbe87512b09f0edb80070e
tiMorpheus <timur10645@icloud.com>
2017-03-07 19:58:49 +0200

    parser2

--- lab2/src/main/java/timur/logic/Parser.java
+++ lab2/src/main/java/timur/logic/Parser.java
@@ -10,23 +10,17 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Parser {
-
     public static List<Sentence> parse(String text) {
-        List<Sentence> sentences = new ArrayList<>();
-        List<PartOfSentence> parts = new ArrayList<>();
-
-
-        StringTokenizer st = new StringTokenizer(text, " \t\n\r:;.!?,/\\|\"\'", true);
-
+        List<Sentence> sentences = new ArrayList<Sentence>();
+        List<PartOfSentence> parts = new ArrayList<PartOfSentence>();
+        StringTokenizer st = new StringTokenizer(text, " \t\n\r:;.!?,/\\|\"\'",
+                true);
 
         while (st.hasMoreTokens()) {
             String s = st.nextToken().trim();
             int size = s.length();
-
-            if (size < 1) {
+            if (size < 1)
                 continue;
-            }
-
             if (size == 1) {
                 switch (s.charAt(0)) {
                     case ' ':
@@ -42,24 +36,23 @@ public class Parser {
                     case '?':
                     case '!':
                         parts.add(new Punctuation(s.charAt(0)));
-
                         Sentence buf = new Sentence(parts);
-
-                        if (buf.getWords().isEmpty()) {
+                        if (buf.getWords().size() != 0) {
                             sentences.add(buf);
-                            parts = new ArrayList<>();
+                            parts = new ArrayList<PartOfSentence>();
                         } else
                             parts.add(new Punctuation(s.charAt(0)));
                         break;
                     default:
                         parts.add(new Word(s.toCharArray()));
                 }
+
             } else {
                 parts.add(new Word(s.toCharArray()));
             }
         }
-
         return sentences;
+
     }
 
 }
